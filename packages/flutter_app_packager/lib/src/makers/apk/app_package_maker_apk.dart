8fe1e6398f6a8b0cfbda505a81acc7c6d5089b7a
pluralplay <vayudanov@ya.ru>
2025-11-09 00:38:31 +0300

    remove version from artifact add universal apk

--- packages/flutter_app_packager/lib/src/makers/apk/app_package_maker_apk.dart
+++ packages/flutter_app_packager/lib/src/makers/apk/app_package_maker_apk.dart
@@ -13,14 +13,21 @@ class AppPackageMakerApk extends AppPackageMaker {
   @override
   Future<MakeResult> make(MakeConfig config) {
     for (final file in config.buildOutputFiles) {
-      final splits = file.uri.pathSegments.last.split('-');
+      final fileName = file.uri.pathSegments.last;
+      final splits = fileName.split('-');
+      final outputPath = config.outputFile.path;
+      final lastDotIndex = outputPath.lastIndexOf('.');
+      final firstPart = outputPath.substring(0, lastDotIndex);
+      final lastPart = outputPath.substring(lastDotIndex + 1);
+      
       if (splits.length > 2) {
+        // Split APK (contains architecture: armeabi-v7a, arm64-v8a, x86_64)
         final sublist = splits.sublist(1, splits.length - 1);
-        final outputPath = config.outputFile.path;
-        final lastDotIndex = outputPath.lastIndexOf('.');
-        final firstPart = outputPath.substring(0, lastDotIndex);
-        final lastPart = outputPath.substring(lastDotIndex + 1);
         final output = '$firstPart-${sublist.join('-')}.${lastPart}';
+        file.copySync(output);
+      } else {
+        // Universal APK (no architecture in filename)
+        final output = '$firstPart-universal.${lastPart}';
         file.copySync(output);
       }
     }
